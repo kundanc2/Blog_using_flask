8f8fa34f7ab0ce386c85b90b35ccd775ab6bfbbc
Kundan Choudhary <kundankumar02002@gmail.com>
2024-06-13 18:18:23 +0530

    Home page completed

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    posts=requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("index.html",posts=posts)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
